ee9e7808645dc2f6a0dbea770fca25ef2b7d286d
Molikkidd <mzkidd5@gmail.com>
2021-10-01 15:21:49 -0700

    add .env

--- playground.js
+++ playground.js
@@ -1,32 +1,18 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const express = require('express');
 const {User} = require('./models');
-const {Bio} = require('./models');
-const JWT_SECRET = process.env.JWT_SECRET;
-const MONGO_URI = process.env.MONGO_URI;
-const CLIENT_PASSWORD = process.env.CLIENT_PASSWORD;
+const password = process.env.CLIENT_PASSWORD;
 const bcrypt = require('bcrypt');
-let salt = bcrypt.genSalt(10);
 const faker = require("faker");
 
+console.log('process', process.env)
 /* mySeedScript.js */
-
-// function randomIntFromInterval(min, max) { // min and max included
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
 async function seedDB() {
-    // Connection URL 
-    // Mongoose connection object
-    // const client = MongoClient(process.env.MONGO_URI, {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    //     useFindAndModify: true
-    // });
-    
     try {
         
+        let salt = await bcrypt.genSalt(10);
+        let hash = await bcrypt.hash(password, salt);
+
         console.log("Connected correctly to server");
         
         // make a bunch of time series data
@@ -38,8 +24,6 @@ async function seedDB() {
             const email = faker.internet.email();
             const password = CLIENT_PASSWORD;
             
-            let hash = bcrypt.hash(password, salt);
-
             let fakeClients = {
                 firstName: firstName ,
                 lastName: lastName,
@@ -60,10 +44,9 @@ async function seedDB() {
                 ],
             };
             fakerClientsArray.push(fakeClients)
-            console.log(fakerClientsArray);
         }
-        // User.collection.insertMany(fakerClientsArray);
-        
+        console.log(fakerClientsArray);
+        User.insertMany(fakerClientsArray);
         console.log("Database seeded! :)");
     } catch (err) {
         console.log(err.stack);
@@ -71,6 +54,7 @@ async function seedDB() {
 }
 
 seedDB();
+
 (async function findAllUsers(id) {
         try {
             // find customer first
